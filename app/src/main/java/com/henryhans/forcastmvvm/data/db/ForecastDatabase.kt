f262811e03c0ff41487894c280de06130ce73ea8
Henry Han <henry.hans.cc@gmail.com>
2020-09-12 14:50:57 -0700

    Format it

--- app/src/main/java/com/henryhans/forcastmvvm/data/db/ForecastDatabase.kt
+++ app/src/main/java/com/henryhans/forcastmvvm/data/db/ForecastDatabase.kt
@@ -20,7 +20,10 @@ abstract class ForecastDatabase : RoomDatabase(){
         private val LOCK = Any()
 
         operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
-            instance ?: buildDatabase(context).also { instance = it }
+            instance ?: buildDatabase(context)
+                .also {
+                    instance = it
+                }
         }
 
         private fun buildDatabase(context: Context)  =
